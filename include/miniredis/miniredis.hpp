06b95ef4154f060e70c945438be1a0f86761401e
Leonardo Freitas <lfreitas.lpf@gmail.com>
2025-05-31 09:39:33 -0300

    chore: add hello world library code and update CMakeLists.txt
    Signed-off-by: Leonardo Freitas <lfreitas.lpf@gmail.com>

---
+++ include/miniredis/miniredis.hpp
@@ -0,0 +1,12 @@
+//
+// Created by Leonardo Freitas on 30/05/25.
+//
+
+#ifndef MINIREDIS_HPP
+#define MINIREDIS_HPP
+
+#include <string>
+
+void hello(const std::string& name);
+
+#endif //MINIREDIS_HPP
---
+++ src/miniredis.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Leonardo Freitas on 30/05/25.
+//
+
+#include "../include/miniredis/miniredis.hpp"
+
+#include <iostream>
+
+void hello(const std::string& name)
+{
+    std::cout << "Hello, " << name << "!" << std::endl;
+}
